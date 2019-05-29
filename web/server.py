d6b69ee0d65e2e90238bf748ad67f5a845ee8feb
jpmiraval <40766473+jpmiraval@users.noreply.github.com>
2019-05-29 10:51:40 -0500

    Add files via upload

--- web/server.py
+++ web/server.py
@@ -12,39 +12,76 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/static/<content>')
+def static_content(content):
+    return render_template(content)
 
-@app.route('/books')
-def books():
+@app.route('/users', methods = ['POST'])
+def create_user():
+    c =  json.loads(request.form['values'])
+    user = entities.User(
+        username=c['username'],
+        name=c['name'],
+        fullname=c['fullname'],
+        password=c['password']
+    )
+    session = db.getSession(engine)
+    session.add(user)
+    session.commit()
+    return 'Created User'
+
+@app.route('/users', methods = ['PUT'])
+def update_user():
+    session = db.getSession(engine)
+    id = request.form['key']
+    user = session.query(entities.User).filter(entities.User.id == id).first()
+    c =  json.loads(request.form['values'])
+    for key in c.keys():
+        setattr(user, key, c[key])
+    session.add(user)
+    session.commit()
+    return 'Updated User'
+
+
+@app.route('/users', methods = ['GET'])
+def get_users():
+    session = db.getSession(engine)
+    dbResponse = session.query(entities.User)
+    data = []
+    for user in dbResponse:
+        data.append(user)
+    return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype='application/json')
+
+
+@app.route('/users/<id>', methods = ['GET'])
+def get_user(id):
     db_session = db.getSession(engine)
-    users = db_session.query(entities.Book)
-    data = users[:]
-    return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype = 'application/json')
+    users = db_session.query(entities.User).filter(entities.User.id == id)
+    for user in users:
+        js = json.dumps(user, cls=connector.AlchemyEncoder)
+        return  Response(js, status=200, mimetype='application/json')
 
-@app.route('/users')
-def users():
-    db_session = db.getSession(engine)
-    users = db_session.query(entities.User)
-    data = users[:]
-    return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype = 'application/json')
-
-
-@app.route('/create_test_books', methods = ['GET'])
-def create_test_books():
-    db_session = db.getSession(engine)
-    book = entities.Book(name="Head First HTML5", isbn="12345", title="Head first about HTML5")
-    db_session.add(book)
-    db_session.commit()
-    return "Test books created!"
-
+    message = { 'status': 404, 'message': 'Not Found'}
+    return Response(message, status=404, mimetype='application/json')
 
 @app.route('/create_test_users', methods = ['GET'])
 def create_test_users():
     db_session = db.getSession(engine)
-    user = entities.User(name="David", apellido="Lazo", codigo="1234", password="qwerty")
+    user = entities.User(name="David", fullname="Lazo", password="1234", username="qwerty")
     db_session.add(user)
     db_session.commit()
     return "Test user created!"
 
+@app.route('/users', methods = ['DELETE'])
+def delete_user():
+    id = request.form['key']
+    session = db.getSession(engine)
+    users = session.query(entities.User).filter(entities.User.id == id)
+    for user in users:
+        session.delete(user)
+    session.commit()
+    return "Deleted Message"
+
 if __name__ == '__main__':
     app.secret_key = ".."
-    app.run(port=8080, threaded=True, host=('0.0.0.0'))
+    app.run(port=8080, threaded=True, host=('127.0.0.1'))
